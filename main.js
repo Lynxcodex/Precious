7f258c269f2dd2a1c3d0df57b4badeaa532e7f51
ILORI PRECIOUS <iloriprecious4@gmail.com>
2025-01-10 21:23:27 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,20 @@
 function downloadFile() {
   location.href='Lynx.pdf';
 }
+
+function hamburg() {
+  document.getElementById("dropdown").style.transform = "translateY(0)";
+}
+function cancel() {
+  document.getElementById("dropdown").style.transform = "translateY(-500px)";
+}
+
+
+const dropdownLinks = document.querySelectorAll('.dropdown .links a');
+const dropdown = document.getElementById('dropdown');
+
+dropdownLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        dropdown.style.transform = 'translateY(-500px)'; // Hides dropdown
+    });
+});
