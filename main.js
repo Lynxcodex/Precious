d84512f2e37d87a24ada1bf32a20adfef54af625
ILORI PRECIOUS <iloriprecious4@gmail.com>
2025-04-28 14:34:06 +0100

    Update main.js

--- main.js
+++ main.js
@@ -17,3 +17,24 @@ dropdownLinks.forEach(link => {
         dropdown.style.transform = 'translateY(-500px)'; // Hides dropdown
     });
 });
+
+
+
+// Show the back-to-top button when scrolling down
+window.onscroll = function () {
+  let backToTopButton = document.getElementById("backToTop");
+
+  if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
+    backToTopButton.style.display = "flex";
+  } else {
+    backToTopButton.style.display = "none";
+  }
+};
+
+// Function to scroll to the top smoothly
+function scrollToTop() {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  });
+}
